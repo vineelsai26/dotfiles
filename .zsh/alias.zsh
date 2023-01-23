9279cc7af761a291418a66745c62378ecd36d0b3
Vineel Sai <mail@vineelsai.com>
2023-01-23 11:19:05 +0530

    new aliases and other small changes

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -5,3 +5,22 @@ alias la='ls -A'
 alias l='ls -CF'
 alias vim='nvim'
 alias rf='rm -rf'
+
+if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+else
+    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+    OS=$(uname -s)
+    VER=$(uname -r)
+fi
+
+if [[ $OS == *"Fedora"* ]]; then
+    alias up='sudo dnf update'
+elif [[ $OS == *"Ubuntu"* ]]; then
+    alias up='sudo apt update && apt upgrade'
+elif [[ $OS == *"Debian"* ]]; then
+    alias up='sudo apt update && apt upgrade'
+fi
