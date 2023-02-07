6a6ee48d0ee0213a7c23d85afa81d4f29637729f
Vineel Sai <mail@vineelsai.com>
2023-02-07 20:15:44 +0530

    add windows dotfiles

---
+++ .windows/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,147 @@
+# Import Terminal Icons
+Import-Module -Name Terminal-Icons
+
+# Find out if the current user identity is elevated (has admin rights)
+$identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+$principal = New-Object Security.Principal.WindowsPrincipal $identity
+$isAdmin = $principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+
+# If so and the current host is a command line, then change to red color 
+# as warning to user that they are operating in an elevated context
+# Useful shortcuts for traversing directories
+function cd... { Set-Location ..\.. }
+function cd.... { Set-Location ..\..\.. }
+
+# Compute file hashes - useful for checking successful downloads 
+function md5 { Get-FileHash -Algorithm MD5 $args }
+function sha1 { Get-FileHash -Algorithm SHA1 $args }
+function sha256 { Get-FileHash -Algorithm SHA256 $args }
+
+# Quick shortcut to start notepad
+function n { notepad $args }
+
+# Quick shortcut to start vscode
+function c { 
+    if ($args.Count -gt 0) {
+        code $args
+    }
+    else {
+        code .
+    }
+}
+
+# Drive shortcuts
+function HKLM: { Set-Location HKLM: }
+function HKCU: { Set-Location HKCU: }
+function Env: { Set-Location Env: }
+
+# Creates drive shortcut for Work Folders, if current user account is using it
+if (Test-Path "$env:USERPROFILE\Work Folders") {
+    New-PSDrive -Name Work -PSProvider FileSystem -Root "$env:USERPROFILE\Work Folders" -Description "Work Folders"
+    function Work: { Set-Location Work: }
+}
+
+$Host.UI.RawUI.WindowTitle = "PowerShell {0}" -f $PSVersionTable.PSVersion.ToString()
+if ($isAdmin) {
+    $Host.UI.RawUI.WindowTitle += " [ADMIN]"
+}
+
+# Does the the rough equivalent of dir /s /b. For example, dirs *.png is dir /s /b *.png
+function dirs {
+    if ($args.Count -gt 0) {
+        Get-ChildItem -Recurse -Include "$args" | Foreach-Object FullName
+    }
+    else {
+        Get-ChildItem -Recurse | Foreach-Object FullName
+    }
+}
+
+# Simple function to start a new elevated process. If arguments are supplied then 
+# a single command is started with admin rights; if not then a new admin instance
+# of PowerShell is started.
+function admin {
+    if ($args.Count -gt 0) {   
+        $argList = "& '" + $args + "'"
+        Start-Process powershell -Verb runAs -ArgumentList $argList
+    }
+    else {
+        Start-Process powershell.exe -Verb runAs
+    }
+}
+
+# Set UNIX-like aliases for the admin command, so sudo <command> will run the command
+# with elevated rights. 
+Set-Alias -Name sudo -Value admin
+
+
+# Make it easy to edit this profile once it's installed
+function Edit-Profile {
+    if ($host.Name -match "ise") {
+        $psISE.CurrentPowerShellTab.Files.Add($profile)
+    }
+    else {
+        code $profile
+    }
+}
+
+# We don't need these any more; they were just temporary variables to get to $isAdmin. 
+# Delete them to prevent cluttering up the user profile. 
+Remove-Variable identity
+Remove-Variable principal
+
+function ip {
+    (Invoke-WebRequest 'http://ifconfig.me/ip').Content
+}
+
+function where($name) {
+    Get-ChildItem -recurse -filter "*${name}*" -ErrorAction SilentlyContinue | ForEach-Object {
+        $place_path = $_.directory
+        Write-Output "${place_path}\${_}"
+    }
+}
+
+function unzip ($file) {
+    Write-Output("Extracting", $file, "to", $pwd)
+    $fullFile = Get-ChildItem -Path $pwd -Filter .\cove.zip | ForEach-Object { $_.FullName }
+    Expand-Archive -Path $fullFile -DestinationPath $pwd
+}
+
+function grep($regex, $dir) {
+    if ( $dir ) {
+        Get-ChildItem $dir | select-string $regex
+        return
+    }
+    $input | select-string $regex
+}
+
+function touch($file) {
+    "" | Out-File $file -Encoding ASCII
+}
+
+function df {
+    get-volume
+}
+
+function sed($file, $find, $replace) {
+    (Get-Content $file).replace("$find", $replace) | Set-Content $file
+}
+
+function which($name) {
+    Get-Command $name | Select-Object -ExpandProperty Definition
+}
+
+function export($name, $value) {
+    set-item -force -path "env:$name" -value $value;
+}
+
+function pkill($name) {
+    Get-Process $name -ErrorAction SilentlyContinue | Stop-Process
+}
+
+function pgrep($name) {
+    Get-Process $name
+}
+
+$ENV:STARSHIP_CONFIG = "$HOME\Documents\Starship\config.toml"
+$ENV:STARSHIP_DISTRO = "者 "
+Invoke-Expression (&starship init powershell)
