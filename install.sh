6cb33b0ce12bf89ff2bce39fc9b0f2c2aef3250b
Vineel Sai <mail@vineelsai.com>
2023-03-27 12:49:42 +0530

    bash history to zsh history

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
+#!/bin/bash
+
+current=$pwd
 git clone --bare https://github.com/vineelsai26/dotfiles $HOME/.cfg
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME stash
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME reset --hard
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME submodule init
 git --git-dir=$HOME/.cfg/ --work-tree=$HOME submodule update
-cd .oh-my-zsh
+cd $HOME/.oh-my-zsh
 git submodule init
 git submodule update
-cd ~
+cd $current
 chsh -s /bin/zsh
 zsh
+unsetopt EXTENDED_HISTORY
+cat $HOME/.bash_history >> $HOME/.zsh_history
