a94df3d7a93e06e2a684de44945debac5ae7d90f
Vineel Sai <mail@vineelsai.com>
2023-04-15 21:44:22 +0530

    fix error messages

---
+++ .zsh/path.zsh
@@ -0,0 +1,31 @@
+# Python User Script Directory
+if [ -d "$HOME/.local/bin" ]; then
+    export PATH="$HOME/.local/bin:$PATH"
+fi
+
+# BUN Auto completions
+[ -s "/home/vineel/.bun/_bun" ] && source "/home/vineel/.bun/_bun"
+
+# BUN Install Directory
+export BUN_INSTALL="$HOME/.bun"
+
+# BUN Directory
+if [ -d "$BUN_INSTALL/bin:$PATH" ]; then
+    export PATH="$BUN_INSTALL/bin:$PATH"
+fi
+
+# Homebrew Directory
+if [ -d "$(brew --prefix)/opt/python@3.11/libexec/bin:$PATH" ]; then
+    export PATH="$(brew --prefix)/opt/python@3.11/libexec/bin:$PATH"
+fi
+
+# VMN Directory
+if [ -d "$HOME/Personal/VMN:$PATH" ]; then
+    export PATH="$HOME/Personal/VMN:$PATH"
+fi
+
+# Python User Script Directory for MacOS
+if [ -d "$HOME/Library/Python/3.11/bin:$PATH" ]; then
+    export PATH="$HOME/Library/Python/3.11/bin:$PATH"
+fi
+
--- .zshrc
+++ .zshrc
@@ -9,19 +9,9 @@ source $ZSH/oh-my-zsh.sh
 source $HOME/.zsh/alias.zsh
 source $HOME/.zsh/nvm.zsh
 source $HOME/.zsh/gpg.zsh
-
-# bun completions
-[ -s "/home/vineel/.bun/_bun" ] && source "/home/vineel/.bun/_bun"
-
-# bun
-export BUN_INSTALL="$HOME/.bun"
+source $HOME/.zsh/path.zsh
 
 test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
-
-# PATH
-export PATH="$HOME/.local/bin:$BUN_INSTALL/bin:$PATH"
-export PATH="$(brew --prefix)/opt/python@3.11/libexec/bin:$PATH"
-export PATH="$HOME/Personal/VMN:$PATH"
 
 #VMN 
 eval "`vmn env`"
